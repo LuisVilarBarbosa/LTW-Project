22c6cc34d46f59162c8a98d744f6333552fccc3e
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2016-12-09 23:55:55 +0000

    Lost changes added

--- database.sql
+++ database.sql
@@ -1,22 +1,24 @@
-CREATE TABLE Restaurant(
+CREATE TABLE restaurants(
 	restaurantId	INTEGER PRIMARY KEY AUTOINCREMENT,
-	name			TEXT	NOT NULL,
-	description		TEXT,
-	image_dir			TEXT,
-	address			TEXT);
+	name		TEXT	NOT NULL,
+	description	TEXT,
+	image_dir	TEXT,
+	address		TEXT,
+	ownerId		INTEGER	NOT NULL,
+	FOREIGN KEY(ownerId) REFERENCES User(userId));
 
-CREATE TABLE User(
-	userId	        INTEGER	PRIMARY KEY AUTOINCREMENT,
-	name 			TEXT,
-	image_dir			TEXT,
-	user			TEXT	NOT NULL,
-	password		TEXT	NOT NULL);
+CREATE TABLE users(
+	userId		INTEGER	PRIMARY KEY AUTOINCREMENT,
+	name 		TEXT,
+	image_dir	TEXT,
+	user		TEXT	NOT NULL,
+	password	TEXT	NOT NULL);
 
-CREATE TABLE RestaurantComment(
-	commentId		INTEGER	PRIMARY KEY AUTOINCREMENT,
-	userId			TEXT	NOT NULL,
+CREATE TABLE restaurantReviews(
+	commentId	INTEGER	PRIMARY KEY AUTOINCREMENT,
+	userId		TEXT	NOT NULL,
 	restaurantId	TEXT	NOT NULL,
 	comment		TEXT,
-	score			INTEGER,
+	score		INTEGER	NOT NULL,
 	FOREIGN KEY(userId) REFERENCES User(userId),
 	FOREIGN KEY(restaurantId) REFERENCES Restaurant(restaurantId));
--- templates/add_restaurant.php
+++ templates/add_restaurant.php
@@ -1,9 +1,13 @@
-<form action="add_restaurant_to_db.php" method="post">
-  Name:<br>
-  <input type="text" name="name"><br>
-  Description:<br>
-  <input type="text" name="description"><br>
-  Image:<br>
-  <input type="url" name="image_dir"><br>
+<form action="action_add_restaurant.php" method="post" method="multipart/form-data">
+  <label for="name">Name: </label>
+  <input type="text" name="name"><br><br>
+  <label for="description">Description: </label>
+  <input type="text" name="description"><br><br>
+  <label for="image_url">Image URL: </label>
+  <input type="url" name="image_url"><br><br>
+  <label for="image_file">Image file: </label>
+  <input type="file" name="image_file"><br><br>
+  <label for="address">Address: </label>
+  <input type="text" name="address"><br><br>
   <input type="submit" name="submit" value="Submit">
 </form>
