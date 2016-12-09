5e31d6163be8a2ef8fa357ad1d8c49cafda1b627
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2016-12-09 23:56:46 +0000

    Owner added to restaurants

--- action_add_restaurant.php
+++ action_add_restaurant.php
@@ -5,6 +5,7 @@
     if (trim($_POST['image_url']) == '' && $_FILES['image_file']['name'] == '') array_push($errors, 'Image URL or image file is mandatory.');
     if (trim($_POST['image_url']) != '' && $_FILES['image_file']['name'] != '') array_push($errors, 'Only one field must be filled (image URL or image file).');
     if (trim($_POST['address']) == '') array_push($errors, 'Address is mandatory.');
+    if (!isset($_GET['ownerId'])) array_push($errors, 'ownerId not set.');
       
     if (sizeof($errors) != 0) {
       foreach($ret['messages'] as $m)
@@ -17,16 +18,16 @@
 
       try {
         if (trim($_POST['image_url']) != '') {
-          addRestaurant($dbh, $_POST['name'], $_POST['description'], $_POST['image_url'], $_POST['address']);
-          $array = getRestaurantId($dbh, $_POST['name'], $_POST['description'], $_POST['image_url'], $_POST['address']);
+          addRestaurant($dbh, $_POST['name'], $_POST['description'], $_POST['image_url'], $_POST['address'], $_GET['ownerId']);
+          $array = getRestaurantId($dbh, $_POST['name'], $_POST['description'], $_POST['image_url'], $_POST['address'], $_GET['ownerId']);
         }
         else if ($_FILES['image_file']['name'] != '') {
           $target_dir = load_image($_FILES['image_file']);
           if($target_dir === false)
             echo "File is not an image.<br>";
           else {
-  	    addRestaurant($dbh, $_POST['name'], $_POST['description'], $target_dir, $_POST['address']);
-  	    $array = getRestaurantId($dbh, $_POST['name'], $_POST['description'], $target_dir, $_POST['address']);
+  	    addRestaurant($dbh, $_POST['name'], $_POST['description'], $target_dir, $_POST['address'], $_GET['ownerId']);
+  	    $array = getRestaurantId($dbh, $_POST['name'], $_POST['description'], $target_dir, $_POST['address'], $_GET['ownerId']);
           }
          }
          else
