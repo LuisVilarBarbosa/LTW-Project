c5e2e19a5ce623e0c619ae4279fc220f4a4e46af
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2016-12-10 00:05:34 +0000

    'getRestaurantsByOwner' added and bug fix

--- database/restaurant.php
+++ database/restaurant.php
@@ -5,8 +5,14 @@
   }
 
   function getRestaurantId($dbh, $name, $description, $image_dir, $address, $ownerId) {
-    $stmt = $dbh->prepare('SELECT restaurantId FROM Restaurant WHERE name = ? AND description = ? AND image_dir = ? AND address = ? AND ownerId = ?');
+    $stmt = $dbh->prepare('SELECT restaurantId FROM restaurants WHERE name = ? AND description = ? AND image_dir = ? AND address = ? AND ownerId = ?');
     $stmt->execute(array($name, $description, $image_dir, $address, $ownerId));
     return $stmt->fetch();	// what if there are more than one restaurant with this values?
+  }
+
+  function getRestaurantsByOwner($dbh, $ownerId) {
+    $stmt = $dbh->prepare('SELECT * FROM restaurants WHERE ownerId = ?');
+    $stmt->execute(array($ownerId));
+    return $stmt->fetchAll();
   }
 ?>
