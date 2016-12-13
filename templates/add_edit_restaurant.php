8878b7dbc830c02c31b948633a6ee0c7622c4038
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2016-12-13 17:57:54 +0000

    'add_edit_restaurant' template bug fixes

--- templates/add_edit_restaurant.php
+++ templates/add_edit_restaurant.php
@@ -1,13 +1,13 @@
-<form action="action_add_edit_restaurant.php" method="post" method="multipart/form-data">
+<form action="action_add_edit_restaurant.php" method="post" enctype="multipart/form-data">
   <label for="name">Name: </label>
-  <input type="text" name="name"><br><br>
+  <input type="text" name="name" /><br /><br />
   <label for="description">Description: </label>
-  <input type="text" name="description"><br><br>
+  <input type="text" name="description" /><br /><br />
   <label for="image_url">Image URL: </label>
-  <input type="url" name="image_url"><br><br>
+  <input type="url" name="image_url" /><br /><br />
   <label for="image_file">Image file: </label>
-  <input type="file" name="image_file"><br><br>
+  <input type="file" name="image_file" /><br /><br />
   <label for="address">Address: </label>
-  <input type="text" name="address"><br><br>
-  <input type="submit" name="submit" value="Submit">
+  <input type="text" name="address" /><br /><br />
+  <input type="submit" name="submit" value="Submit" />
 </form>
