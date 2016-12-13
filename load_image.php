86859a350e7fbee7c909ad03de7a720674d4903c
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2016-12-13 22:50:35 +0000

    Files uploading works

--- load_image.php
+++ load_image.php
@@ -2,17 +2,15 @@
   function load_image($image /* from $_FILES */) {
     $target_dir = "uploads/";
     $target_file = $target_dir . basename($image['name']);
-    $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 
     // Check if image file is an actual image or fake image
     $check = getimagesize($image['tmp_name']); // $check has image info
     if($check !== false) {
-      move_uploaded_file($image['tmp_name'], $target_dir);
-      $uploadOk = true;
+      if(!move_uploaded_file($image['tmp_name'], $target_file))
+        die('Error storing image.');
     } else
-      $uploadOk = false;
+      die('Not an image.');
 
-    return $uploadOk && $target_dir;
+    return $target_file;
   }
-    
 ?>
