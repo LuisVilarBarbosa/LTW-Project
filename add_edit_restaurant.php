a37e7f1a02f5e54f84578d0d92d57c3b762aae73
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2016-12-15 22:46:46 +0000

    try/catch added

--- add_edit_restaurant.php
+++ add_edit_restaurant.php
@@ -3,10 +3,14 @@
   include('verify_session.php');
   include('database/restaurant.php');
 
-  if(isset($_GET['restaurantId']))
-    $restaurant = getRestaurantById($_GET['restaurantId']);
-  else
-    $restaurant = NULL;
+  try {
+    if(isset($_GET['restaurantId']))
+      $restaurant = getRestaurantById($_GET['restaurantId']);
+    else
+      $restaurant = NULL;
+  } catch (PDOException $e) {
+    array_push($_SESSION['error_messages'], $e->getMessage());
+  }
 
   include('templates/header.php');
   include('templates/add_edit_restaurant.php');
