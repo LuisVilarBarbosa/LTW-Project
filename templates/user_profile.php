8db025e04ef8e0554811b5fd2ca8b929b5e97bb3
rpedro10 <rpedro10@gmail.com>
2016-12-15 12:36:27 +0000

    user_profile now shows correctly users info
    falta mostrar restaurantes

--- templates/user_profile.php
+++ templates/user_profile.php
@@ -1,4 +1,34 @@
-﻿<div id="id01" class="modal">
+﻿<!DOCTYPE html
+          PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+          "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<link rel="stylesheet" type="text/css" href="css/start_page.css" />
+
+<h1><b>  <?php
+          $userId =  $_GET['id'];
+        //    echo $userId ;
+        //   $user = array();
+          //$_POST=getUserById($userId);
+
+      // $user = getUsersById($_SESSION['$userId']);
+      // print_r($user]);
+        //echo $user['name'];
+        // print_r('$user['name']'');
+//print_r($firstquarter);
+
+  $stmt = $dbh->prepare('SELECT * FROM users WHERE userId = ?');
+  $stmt->execute(array($userId));
+  $user= $stmt->fetch();
+//echo $user['username'];
+
+ ?>
+  </b></h1>
+
+
+
+
+
+<div id="id01" class="modal">
     <form class="modal-content animate" action="action_add_edit_user.php" method="post">
         <div class="imgcontainer">
             <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
@@ -6,13 +36,17 @@
         </div>
         <div class="container">
             <label><b>Username</b></label>
-            <input type="text" placeholder="stuff" name="username" required=required />
+            <input type="text" placeholder="<?php
+                        echo $user['username'];
+             ?> " name="username" required=required />
             <label><b>Password</b></label>
-            <input type="password" placeholder="stuff" name="password" required=required />
+            <input type="password" placeholder="nova password" name="password" required=required />
             <label><b>Nome</b></label>
-            <input type="text" placeholder="   some stuff" name ="name" required=required />
+            <input type="text" placeholder="  <?php
+                        echo $user['name'];
+             ?> " name ="name" required=required />
             <label><b>Imagem</b></label>
-            <input type="text" placeholder="imagem " name="image" required=required />
+            <input type="file" placeholder="Enter image" name="image" required="required" />
             <button type="submit">Alterar</button>
         </div>
         <div class="container" style="background-color:#f1f1f1">
@@ -41,13 +75,22 @@
 </div>
 <br />
 <input type="text" name="search" placeholder="Search.." />
+
+
 <h2>Utilizador</h2>
 <div class="card">
-    <img src="../resources/example_user.jpg" alt="Avatar" style="width:100%" />
+    <img src=" <?php
+                echo $user['image_dir'];
+     ?> " alt="Avatar" style="width:100%" />
     <div class="container">
 
-        <h4><b>GetNome   </b></h4>
-        <p>get username</p>
+        <h3><b> <?php
+                    echo $user['name'];
+         ?>   </b></h3>
+        <p> <?php
+
+                    echo $user['username'];
+         ?> </p>
     </div>
 </div>
 <br />
@@ -55,50 +98,3 @@
 <button onclick="document.getElementById('id02').style.display='block'" >Adicionar Restaurante</button>
 <br />
 <br />
-<ul class="tab">
-    <li><a href="javascript:void(0)" class="tablinks" onclick="openCity(event, 'London')">Restaurantes</a></li>
-</ul>
-<div id="London" class="tabcontent">
-    <p>Lista de restaurantes do utilizador:</p>
-</div>
-<br />
-<br />
-<script>
-    function openCity(evt, cityName) {
-        var i, tabcontent, tablinks;
-        tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
-        }
-        tablinks = document.getElementsByClassName("tablinks");
-        for (i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
-        document.getElementById(cityName).style.display = "block";
-        evt.currentTarget.className += " active";
-    }
-</script>
-
-<script>
-    // Get the modal
-    var modal = document.getElementById('id01');
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-</script>
-
-<script>
-    // Get the modal
-    var modal = document.getElementById('id02');
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-</script>
