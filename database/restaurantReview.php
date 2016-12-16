3dbcf5309d33c8e1f9d48f3e56f41cbf1fb0165e
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2016-12-16 00:41:41 +0000

    restaurantReviews function added

--- database/restaurantReview.php
+++ database/restaurantReview.php
@@ -4,4 +4,11 @@
     $stmt = $dbh->prepare('INSERT INTO restaurantReviews (userId, restaurantId, comment, score) VALUES (?, ?, ?, ?)');
     $stmt->execute(array($userId, $restaurantId, $comment, $score));
   }
+
+  function getRestaurantReviewsByRestaurantId($restaurantId) {
+    global $dbh;
+    $stmt = $dbh->prepare('SELECT * FROM restaurantReviews WHERE restaurantId = ?)');
+    $stmt->execute(array($restaurantId));
+    return $stmt->fetchAll();
+  }
 ?>
