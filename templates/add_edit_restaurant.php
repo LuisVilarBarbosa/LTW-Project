b55c0ab13510d2b3e4a4b6308be7dbbc553664f2
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2016-12-16 04:39:18 +0000

    '/' that was presented bug fix

--- templates/add_edit_restaurant.php
+++ templates/add_edit_restaurant.php
@@ -4,15 +4,15 @@
     </div>
     <div class="container">
         <label for="name"><b>Name</b></label>
-        <input type="text" placeholder="Restaurant name" name="name" <?php if ($restaurant != NULL) ?> value=<?=$restaurant['name']?> />
+        <input type="text" placeholder="Restaurant name" name="name" <?php if ($restaurant != NULL) { ?> value=<?=$restaurant['name']?> <?php } ?> />
         <label for="description"><b>Description</></label>
-        <input type="text" placeholder="Description" name="description" <?php if ($restaurant != NULL) ?> value=<?=$restaurant['description']?> />
+        <input type="text" placeholder="Description" name="description" <?php if ($restaurant != NULL) { ?> value=<?=$restaurant['description']?> <?php } ?> />
         <label for="image_url"><b>Image URL</b></label>
-        <input type="url" placeholder="Image URL" name="image_url" <?php if ($restaurant != NULL) ?> value=<?=$restaurant['image_dir']?> />
+        <input type="url" placeholder="Image URL" name="image_url" <?php if ($restaurant != NULL) { ?> value=<?=$restaurant['image_dir']?> <?php } ?> />
         <label for="image_file"><b>Image file</b></label>
         <input type="file" name="image_file" />
         <label for="address"><b>Address</b></label>
-        <input type="text" placeholder="Address" name="address" <?php if ($restaurant != NULL) ?> value=<?=$restaurant['address']?> />
+        <input type="text" placeholder="Address" name="address" <?php if ($restaurant != NULL) { ?> value=<?=$restaurant['address']?> <?php } ?> />
         <button type="submit" class="save_button">Add</button>
     </div>
     <div class="cancel_container">
