8b7b44679a1cca4e92611f57e03e98d35b7e5dc7
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2016-12-16 06:44:18 +0000

    'database.sql' updated

--- database.sql
+++ database.sql
@@ -1,3 +1,14 @@
+CREATE TABLE users(
+	userId		INTEGER	PRIMARY KEY AUTOINCREMENT,
+	name 		TEXT,
+	image_dir	TEXT,
+	username		TEXT	NOT NULL UNIQUE,
+	password	TEXT	NOT NULL);
+
+INSERT INTO users VALUES (1,'Fatima Costa','uploads/1-img2.jpg','up123','$2y$15$tz1XHyUOUKlD8Gv2jj2Ktu8J.xUEePZOdWoBkQ4tAhWHbvLVf0DQS');
+INSERT INTO users VALUES (2,'Filipa Nanda','uploads/1-img1.jpeg','filipa','$2y$15$KxdXyC10dR7TbnwPsMLUhuo9ObatqXisB5oJJZUNXNPuOH7fOu54i');
+INSERT INTO users VALUES (3,'Daniela Albertina','uploads/1-example_user.jpg','daniela','$2y$15$BD44.ZYku.PTuxytVwp0r./kFH7lnrXK42muJfomuNgVCms181j/y');
+
 CREATE TABLE restaurants(
 	restaurantId	INTEGER PRIMARY KEY AUTOINCREMENT,
 	name		TEXT	NOT NULL,
@@ -6,13 +17,9 @@ CREATE TABLE restaurants(
 	address		TEXT,
 	ownerId		INTEGER	NOT NULL,
 	FOREIGN KEY(ownerId) REFERENCES User(userId));
-
-CREATE TABLE users(
-	userId		INTEGER	PRIMARY KEY AUTOINCREMENT,
-	name 		TEXT,
-	image_dir	TEXT,
-	username		TEXT	NOT NULL UNIQUE,
-	password	TEXT	NOT NULL);
+	
+INSERT INTO restaurants VALUES (1,'Taberna do ganso','A melhor em Baguim do Monte','https://s-media-cache-ak0.pinimg.com/originals/9a/82/8d/9a828d0fc00a2540d35339e3aba5bfcc.jpg','Rua S.João nº317 chavao',2);
+INSERT INTO restaurants VALUES (2,'Tapas da eira','Nao ha melhores tapas do que aqui','http://www.douropalace.com/Images/Restaurant/Eira-Bar/Gallery/01.jpg','Rua Martim Moniz-Lisboa',2);
 
 CREATE TABLE restaurantReviews(
 	reviewId	INTEGER	PRIMARY KEY AUTOINCREMENT,
@@ -23,3 +30,7 @@ CREATE TABLE restaurantReviews(
 	answer		TEXT,
 	FOREIGN KEY(userId) REFERENCES User(userId),
 	FOREIGN KEY(restaurantId) REFERENCES Restaurant(restaurantId));
+
+INSERT INTO restaurantReviews VALUES (1,'1','2','',3,'Pena nao ter dado mais feedback');
+INSERT INTO restaurantReviews VALUES (2,'3','1','Gostei muito do atendimento',4,'Obrigado');
+INSERT INTO restaurantReviews VALUES (3,'2','2','',5,NULL);
